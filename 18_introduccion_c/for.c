d903e7669f61033b814229889361a78989b13cea
Fernando Novillo <ferchon123443@gmail.com>
2021-05-02 17:29:54 -0500

    Commit con unos cambios en introduccion c

--- 18_introduccion_c/for.c
+++ 18_introduccion_c/for.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 
@@ -15,21 +14,3 @@ int main(int argc, char const *argv[])
 }
 
 
-=======
-#include <stdio.h>
-
-
-
-int main(int argc, char const *argv[])
-{
-    int n  = 20;
-    int i;
-    for (i = 0; i < n; i++)
-
-        printf("el numero actualmente es: %d\n", i );
-
-    return 0;
-}
-
-
->>>>>>> d1bff8aa9e084cc099db80a3377014aa5d90fe12
--- 18_introduccion_c/variables.c
+++ 18_introduccion_c/variables.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include <stdio.h>
 
 //Variable Declarations
@@ -21,28 +21,28 @@ int a, b, c;
     printf("Value of sum is: %f", h);
    
     return 0;
-=======
-#include <stdio.h>
-
-//Variable Declarations
-extern int a, b, c;
-
-float f,g,h;
-
-int main(){
-//Varaible definition
-
-int a, b, c;
-
-//Variable initialization    
-    
-    f = 1000;
-    g = 21447;
-
-    h = f + g;
-
-    printf("Value of sum is: %f", h);
-   
-    return 0;
->>>>>>> d1bff8aa9e084cc099db80a3377014aa5d90fe12
 }
+#include <stdio.h>
+
+//Variable Declarations
+extern int a, b, c;
+
+float f,g,h;
+
+int main(){
+//Varaible definition
+
+int a, b, c;
+/*
+//Variable initialization    
+    
+    f = 1000;
+    g = 21447;
+
+    h = f + g;
+
+    printf("Value of sum is: %f", h);
+   
+    return 0;
+
+}*/
