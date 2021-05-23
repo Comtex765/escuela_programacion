68403387d6c40d3ff8bddd0f44541499197180c1
Fernando Novillo <ferchon123443@gmail.com>
2021-05-23 11:14:43 -0500

    "Commi con avances en el curso de calculo basico"

--- 17_python_intermedio/readAsks.py
+++ 17_python_intermedio/readAsks.py
@@ -1,29 +1,25 @@
 from random import randint
 from time import sleep
+
 def chooseAsk():
     with open("./archivos/lista.txt", "r", encoding="utf-8") as f:
 
         data = [line for line in f]
-        
+
         with open("./archivos/chose.txt", "a", encoding="utf-8") as g:
 
             with open("./archivos/chose.txt", "r", encoding="utf-8") as h:
-                num = randint(0,402)
 
+                num = randint(0,402)
                 numP = str(num) + "\n"
 
                 while True:
                     numsList = [line for line in h]
 
                     if numP in numsList:
-
-                        print("num ",num," en lista")
-
                         while numP in numsList:
-                            print("calculando ",num," en lista")
                             num = randint(0,402)
                             numP = str(num) + "\n"
-
 
                     else:
                         g.write(str(num))
@@ -35,6 +31,7 @@ def showAsk(ask):
     with open("./archivos/pooh.txt","r",encoding="utf-8") as i:
         for line in i:
             print(line,end="")
+
     print("\033[1;33m"+"\n",ask,end="")
 
 
